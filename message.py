90e710513124da5a44bfc33a97300b456e85a092
SupriyaJakkani <jakkanisupriya34@gmail.com>
2025-04-08 19:34:25 +0530

    commit

---
+++ message.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+import time
+import random
+
+animals = ['monkey', 'donkey', 'dog']
+
+time.sleep(8)
+
+for i in range(50):
+    a = random.choice(animals)
+    pg.write(f"You are a {a}")
+    pg.press('enter')
